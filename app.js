5b269565b567adce9f70001d69f74947f5ea216b
Juankr95 <32262525+Juankr95@users.noreply.github.com>
2017-10-26 23:46:38 +0200

    actualización del app.js
    Modificación de la acción mover y empiece de crear un objeto para las tropas

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 
-
  var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'zonaJuego', { preload: preload, create: create, update: update });
    
 
         var mapa = [
-             [1,1,1,1,1,1,2,2,2,2],
-             [1,2,2,2,2,1,1,1,2,2],
-             [1,1,2,2,2,1,1,2,2,1],
-             [2,2,2,2,2,2,2,2,2,1],
-             [1,2,2,2,2,1,1,2,2,1],
-             [1,1,1,1,1,1,2,2,2,1],
-             [1,2,2,2,2,1,1,2,2,1],
-             [1,2,2,2,2,1,1,2,2,1],
-             [1,2,2,2,2,1,1,2,2,1],
-             [1,2,1,2,2,1,1,2,2,1],
+             [1,1,1,1,1,1,1,1,1,1],
+             [1,1,1,1,1,1,1,1,1,1],
+             [1,1,1,1,1,1,1,1,1,1],
+             [1,1,1,1,1,1,1,1,1,1],
+             [1,1,1,1,1,1,1,1,1,1],
+             [1,1,1,1,1,1,1,1,1,1],
+             [1,1,1,2,1,1,1,1,1,1],
+             [1,1,1,1,1,1,1,1,1,1],
+             [1,1,1,1,1,1,1,1,1,1],
+             [1,1,1,1,1,1,1,1,1,1],
          ];
 
         var jugadores = [
@@ -28,7 +27,7 @@
              [1,1,1,1,1,1,1,1,1,1],
              [1,1,1,1,1,1,1,1,1,1],
          ];
-		 
+        
         var tiles = [
              [1,1,1,1,1,1,2,2,2,2],
              [1,2,2,2,2,1,1,1,2,2],
@@ -50,8 +49,12 @@
         var pos;
         var marker;
         var player1;
+        var player2;
         var tiledX;
         var tiledY;
+
+        const ANCHOMAPA = 10;
+        const ALTOMAPA = 10;
 
 
     function preload() {
@@ -61,8 +64,46 @@
         game.load.image('player1click', 'assets/PLayer1Click.png');
         game.load.image('Spitter', 'assets/Spitter.png');
         game.load.image('SpitterClick', 'assets/SpitterClick.png');
+        //game.load.image('prueba', 'assets/Prueba.png');
         
     }
+    function Tropa (id, sprite, x, y, mov, ataque, vida){
+        //var id = id;
+        //var mov = mov;
+        var tropa = game.add.sprite(x, y, sprite);
+        tropa.ataque = 2;
+        var movi = 2;
+        //var ataque = ataque;
+        //var vida = vida;
+        
+        // funciones
+        this.mover = function (x, y) {
+            tropa.x = x;
+            tropa.y = y;
+        };
+        
+        //this.mov = 2;
+        this.getX = function () {
+            return x;
+        };
+        
+        this.getY = function () {
+            return y;
+        };
+        
+        this.mov = function () {
+            return movi;
+        };
+        /*this.setSprite = function(sprite){
+            this.sprite = game.add.sprite(this.x, this.y, sprite);
+        }*/
+        
+        
+    }
+
+
+
+    
 
     function create() {
         /* PINTADO DEL MAPA */
@@ -71,73 +112,102 @@
                 var x = j * anchoTile;
                 var y = i * altoTile;
                 var tileType = mapa[i][j];
-                game.add.sprite(x, y, tileType);
+                tiles[i][j] = game.add.sprite(x, y, tileType);
             }
         }
-       
+
         jugadores[2][5] = 2;
-        
-        //  This creates a simple sprite that is using our loaded image and
-        //  displays it on-screen and assign it to a variable
+        jugadores[4][5] = 2;
+
         player1 =  game.add.sprite(2*anchoTile, 5*altoTile, 'player1');
+        //player2 =  game.add.sprite(4*anchoTile, 5*altoTile, 'player1');
+        
+        //arrayPersonajes = new Array(6);
+        //arrayPersonajes[0] = new Tropa ('alien', 'player1', 5*anchoTile, 5*altoTile, 2, 2, 20);
+        
+        player2 = new Tropa ('alien', 'player1', 5*anchoTile, 5*altoTile, 2, 2, 20);
+        
+        //player2.setSprite('player1');
 
         
 
         //  Enables all kind of input actions on this image (click, etc)
         player1.inputEnabled = true;
+        player2.inputEnabled = true;
 
 
-        player1.events.onInputDown.add(listener, this);
+        player1.events.onInputDown.add(listener, this); //// NO FUNCIONA CON UN OBJETO (se puede poner un sprite constantemente encima?)
 
+        
+        //text = game.add.text(500, 500, '', { fill: '#ffffff' });
+        //text.text = "x:" + pointer.x + " y:" + pointer.y;
   
     }
 
-    function moveSprite(pointer) {
+    /*function atacar(pointer, player){
+        
+        var distancia = player.rango;
+        var daño = player.daño;
+        
+        var posicionX = Math.floor(player2.getX/(100));
+        var posicionY = Math.floor(player2.getY/(60));
+        
+        var ratonX = Math.floor(pointer.x/(100));
+        var ratonY = Math.floor(pointer.y/(60));
+        
+        /////////// copiar y pegar codigo de volver a pintar tiles antiguos
+        
+        if(( (Math.abs(ratonX - posicionX) + Math.abs(ratonY - posicionY)) <= distancia ) && (jugadores[ratonX][ratonY] == 2)){
+            
+            player.setVida(daño);
+            //bloquear boton atacar y contador
+        }
+        
+    }*/
+
+    function moveSprite(pointer, player) {
         
         if(counter != 5){
-        //Tile a la que me quiero mover
-        tiledX = Math.floor(pointer.x/(100));
-        tiledY = Math.floor(pointer.y/(60));
-        
-        actualTiledX = Math.floor(player1.x/(100));
-        actualTiledY = Math.floor(player1.y/(60));
-        
-        //Comprueba que no se salga de las casillas estipuladas
-            if((tiledX <= actualTiledX+1 & tiledX >= actualTiledX-1 & tiledY == actualTiledY) || (tiledY <= actualTiledY+1 & tiledY >= actualTiledY-1 & tiledX == actualTiledX)){
+            //Tile a la que me quiero mover
+            tiledX = Math.floor(pointer.x/(100));
+            tiledY = Math.floor(pointer.y/(60));
 
-                //me lo vuelve a pintar del color original del mapa
-                var posicionX = Math.floor(player1.x/(100));
-                var posicionY = Math.floor(player1.y/(60));
+            actualTiledX = Math.floor(player.x/(100));
+            actualTiledY = Math.floor(player.y/(60));
 
-                var tileType = mapa[posicionY][posicionX+1]; //recupera el tile anterior
-                if((posicionX >= 0) && (posicionY >= 0)) tiles[posicionY][posicionX+1].loadTexture(tileType, 0);
-                
-                tileType = mapa[posicionY][posicionX-1];
-                if((posicionX > 0) && (posicionY >= 0)) tiles[posicionY][posicionX-1].loadTexture(tileType, 0);
-                
-                tileType = mapa[posicionY+1][posicionX];
-                if((posicionX >= 0) && (posicionY >= 0)) tiles[posicionY+1][posicionX].loadTexture(tileType, 0);
-                
-                tileType = mapa[posicionY-1][posicionX];
-                if((posicionX >= 0) && (posicionY > 0)) tiles[posicionY-1][posicionX].loadTexture(tileType, 0);
+            var posicionX = Math.floor(player.x/(100));
+            var posicionY = Math.floor(player.y/(60)); 
 
-                //Actualizo la posición
-                player1.x = tiledX*100;
-                player1.y = tiledY*60;
-                counter++;
-                //text.destroy();
-                cambioEstadoBloqueado($("#Batacar"),true);
+            for (var i=0; i < 10; i++){
+                for (var j= 0; j<10 ; j++){
+                    if((Math.abs(i - actualTiledX) + Math.abs(j - actualTiledY) <= player.mov) && (mapa[i][j]!=2) && ((i == tiledX)&(j == tiledY))){
+                        player.mover(tiledX*100,tiledY*60);
 
-            }else {
-                /* NO SE CONSIGUE BORRAR EL TEXTO UNA VEZ PUESTO */
-                text = game.add.text(500, 500, '', { fill: '#ffffff' });
-                text.text = "Selecciona una casilla válida"; 
+                        counter++;
+                        //text.destroy();
+                        cambioEstadoBloqueado($("#Batacar"),true);
 
+                        for (var k=0; k < 10; k++){
+                            for (var l= 0; l <10 ; l++){
+                                if((Math.abs(k - posicionX) + Math.abs(l - posicionY) <= player.mov)){
+
+                                    tiles[k][l].loadTexture(tileType, 0);
+
+                                }              
+                            }
+                        }
+                    }else {
+                    /* NO SE CONSIGUE BORRAR EL TEXTO UNA VEZ PUESTO */
+                    text = game.add.text(500, 500, '', { fill: '#ffffff' });
+                    text.text = "Selecciona una casilla válida"; 
+
+                    }              
+                }
             }
+
         }
 
     }
-
 
     function listener () {
         
@@ -151,12 +221,11 @@
         
         game.debug.inputInfo(32, 32);
         pos = game.input.activePointer.position;
-
-        game.debug.text("PlayerX:" + player1.x + " PlayerY:" + player1.y + "TileX:" + tiledX + " TileY:" + tiledY, 180, 200);
-        
+        //updateMarker();
+        //var movimiento = arrayPersonajes[0].getX();
+        game.debug.text("PlayerX:" + player1.x + " PlayerY:" + player1.y + "TileX:" + tiledX + " TileY:" + tiledY + " P: ", 180, 200);
 
     }
-
 
 
 // función que permite poner visible o no una sección div, asi conseguimos que
@@ -243,19 +312,28 @@ $(function() {
     $("#Bmover").click(
 			function() {
                     counter = 0;
-                    game.input.onDown.add(moveSprite, this);
+                    game.input.onDown.add(moveSprite, this, player2);
                     
                     //colorea de azul las tiles a las que puedo moverme
-                    var posicionX = Math.floor(player1.x/(100));
-                    var posicionY = Math.floor(player1.y/(60)); 
+                    var posicionX = Math.floor(player2.getX()/(100));
+                    var posicionY = Math.floor(player2.getY()/(60));
 
-                    if((posicionX >= 0) && (posicionY >= 0)) tiles[posicionY][posicionX+1].loadTexture('player1', 0);
-                    if((posicionX >= 0) && (posicionY >= 0)) tiles[posicionY+1][posicionX].loadTexture('player1', 0);
-                    if((posicionX >= 0) && (posicionY > 0)) tiles[posicionY-1][posicionX].loadTexture('player1', 0);
-                    if((posicionX > 0) && (posicionY >= 0)) tiles[posicionY][posicionX-1].loadTexture('player1', 0);
+                    var mov = player2.mov();
+                    for (var k=0; k < 10; k++){
+                        for (var l= 0; l <10 ; l++){
+                            if(( (Math.abs(k - posicionX) + Math.abs(l - posicionY)) <= mov )){
+
+                                tiles[k][l].loadTexture('player1', 0);
+
+                            }              
+                        }
+                    }
+                    
                     
                     //cambioEstadoBloqueado($("#Bmover"),false);
                     cambioEstadoBloqueado($("#Batacar"),false);
 				})
     
 })
+                
+

547c00d81fe7ac023abb7a5471b6a9199e71cf18
AngeelBG <32101206+AngeelBG@users.noreply.github.com>
2017-12-11 17:07:35 +0100

    Actualizacion Application
    Imlementacion para WS

--- Java/Application.java
+++ Java/Application.java
@@ -2,10 +2,25 @@ package es.urjc.code.juegosenred;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @SpringBootApplication
-public class Application {
+@EnableWebSocket
+public class Application implements WebSocketConfigurer {
+	
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(runicHandler(), "/runicwars");
+	}
 
+	@Bean
+	public WebSocketHandler runicHandler() {
+		return new RunicHandler();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
